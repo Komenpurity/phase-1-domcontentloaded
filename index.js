6e52feac85a4344ccd442c48e28c23c30b21d7e8
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-22 18:40:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Your code goes heren
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.getElementById('text').innerHTML = "This is really cool!"
+  });
